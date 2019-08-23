77ff9a8224752454e6c691aae3f989d6a5d084cc
Hanna Lee <mani21na@gmail.com>
2019-08-23 22:12:32 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -20,7 +20,7 @@ class EmailAddressParser
   end
 end
 
-#email_addresses = "john@doe.com, person@somewhere.org"
-#parser = EmailAddressParser.new(email_addresses)
+email_addresses = "john@doe.com, person@somewhere.org"
+parser = EmailAddressParser.new(email_addresses)
  
-#parser.parse
+parser.parse

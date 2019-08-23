aab5c3990acd60a2ff92a1c5a26bed6198cbe290
Hanna Lee <mani21na@gmail.com>
2019-08-23 22:01:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -13,7 +13,7 @@ class EmailAddressParser
   end
   
   def parse
-    parse_emails = email_addresses.split(","||" ")
+    parse_emails = email_addresses.split(","||" ").strip
     parse_emails
     #binding.pry
   end
